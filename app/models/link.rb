70f7865ef46e1b52f5312859dc40e3080a6dd07e
Fajarullah <frullah12@gmail.com>
2021-12-19 17:49:50 +0700

    add annotation using annotate gem

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: links
+#
+#  id         :integer          not null, primary key
+#  url        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Link < ApplicationRecord
   URL_REGEXP = URI::DEFAULT_PARSER.make_regexp(%w[http https])
 
